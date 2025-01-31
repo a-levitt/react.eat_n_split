2f3ad00d854bc33f2a0f1d921bca8fa1e1caed61
Artem Trifonov <artemis.levitt@gmail.com>
2025-01-31 21:14:49 +0100

    Render FormAddFriend if Add friend button clicked or hide

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import FriendsList from "./Components/FriendsList.jsx";
 import FormAddFriend from "./Components/FormAddFriend.jsx";
 import Button from "./Components/Button.jsx";
 import FormSplitBill from "./Components/FormSplitBill.jsx";
+import {useState} from "react";
 
 function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
 
   return (
     <>
       <div className="app">
           <div className="sidebar">
             <FriendsList/>
-            <FormAddFriend/>
-            <Button>Add friend</Button>
+            {showAddFriend && <FormAddFriend/>}
+            <Button onClick={()=>setShowAddFriend(!showAddFriend)}>{showAddFriend ? "Close" : "Add friend"}</Button>
           </div>
           <div>
             <FormSplitBill />
